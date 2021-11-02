ba322ae227f6f6682577bff5d924544bf7489ad0
aboozar <aboozar@gmial.com>
2021-11-03 00:33:40 +0330

    add if test

--- js.js
+++ js.js
@@ -3,3 +3,12 @@
 for (let x = 0; x < 10; x++) {
     console.log(x * 10);
 }
+
+
+// if test
+let num = 12;
+if (num > 20) {
+    console.log('num is bigger');
+} else {
+    console.log('num is smaller');
+}
