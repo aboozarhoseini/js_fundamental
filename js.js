d2f8088b95efd06cd7859fcebe3db89e26f840b2
aboozar <aboozar@gmial.com>
2021-11-03 00:14:53 +0330

    add loop test

--- js.js
+++ js.js
@@ -1,12 +1,5 @@
+
 // this is for loop test
 for (let x = 0; x < 10; x++) {
     console.log(x * 10);
-}
-
-// if test
-let num = 12;
-if (num > 20) {
-    console.log('num is bigger');
-} else {
-    console.log('num is smaller');
 }
